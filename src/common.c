9a16efe5037690e98ca109013f5d2d1c3317a881
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-03-25 19:42:03 +0100

    Add TODOs

--- src/common.c
+++ src/common.c
@@ -12,3 +12,7 @@ last edited: 2025-02-26 21:40:21
 #include "common.h"
 
 //TODO add common functions here
+//TODO add serialize_response e deserialize_request
+//TODO SIMD vectorize headers
+//TODO hashmap with xxhash for headers
+//TODO only deserialize headers the user is intrested to. (he provides a list of expected headers beforehands)
